b80eb898f25b9ac9a759911330cd3024b2a02dbe
Travis <travis.libby@xero.com>
2015-11-06 09:46:08 -0800

    Create config file.

---
+++ api/config.js
@@ -0,0 +1,8 @@
+var yelpCredentials = {
+  consumer_key: 'XMywZGNkpWh27JEtNNt5QQ', 
+  consumer_secret: 'GmgT1BEMZVLOaZW4FRm7Du_nvQo',
+  token: '56HxOnxR_EAtTvHP1a4oW4POGAxceP10',
+  token_secret: 'BVdu_j35wBKNpj83wmafuek8L-Y'
+};
+
+module.exports = yelpCredentials;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var yelp = require('yelp').createClient({
-  consumer_key: 'XMywZGNkpWh27JEtNNt5QQ', 
-  consumer_secret: 'GmgT1BEMZVLOaZW4FRm7Du_nvQo',
-  token: '56HxOnxR_EAtTvHP1a4oW4POGAxceP10',
-  token_secret: 'BVdu_j35wBKNpj83wmafuek8L-Y'
-});
+var yelpCredentials = require('../config');
+var yelp = require('yelp').createClient(yelpCredentials);
 
 router.get('/', function(req, res, next) {
 
